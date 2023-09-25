13be9d216aaa3b099fdd5b0301435d12a23dd418
krutko77 <70036691+krutko77@users.noreply.github.com>
2023-09-25 20:35:41 +0300

    add page about

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,10 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+// Кнопка назад
+if ( document.querySelector('.button-back') ) {  // Проверяем наличие элемента на странице
+	document.querySelector('.button-back').onclick = function(){ // Клик по кнопке НАЗАД
+		window.history.go(-2); // возвращаемся на два шага назад
+	};
+}
