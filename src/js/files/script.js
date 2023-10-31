d69671d2fa68c72a5d4312d8726d0960b6779e68
krutko77 <70036691+krutko77@users.noreply.github.com>
2023-10-31 21:59:21 +0300

    corrected font-size in home

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,6 +3,15 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
+// Отключение transition до полной загрузки страницы
+if ( document.querySelector('.action__link') ) {  //
+	let elem = document.querySelector('.action__link'); 
+
+			window.onload = function() {
+			elem.classList.remove('preload');
+		};	
+}
+
   // Кнопка НАЗАД
 if ( document.querySelector('.button-back') ) {  // Проверяем наличие элемента на странице
 	document.querySelector('.button-back').onclick = function(){ // Клик по кнопке НАЗАД
